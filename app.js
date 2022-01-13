b515a73350fd0d81ede71e811e10548e69ff6a20
“Liv <LReinh@PC7918.nordic.local>
2022-01-13 09:55:43 +0100

    removed useless comments added usefull comments

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Function for adding points in console
+
 function adPointsConsole (onePointers, threePointers){
 let sum= onePointers*2+threePointers*3;
 console.log(sum)
@@ -5,15 +7,11 @@ console.log(sum)
 
 adPointsConsole(2,1)
 
+//function for showing score in the DOM
 
-
-//let numberOfTwoPointers= document.getElementById("two-points").value;
-
-//console.log(numberOfTwoPointers)
 document.getElementById("scoreButton").onclick= function(){
     let twoPoints=document.getElementById("two-points").value
     let threePoints=document.getElementById("three-points").value
-   let sum=twoPoints*2+threePoints*3
-
+    let sum=twoPoints*2+threePoints*3
    document.getElementById("teamScore").innerHTML = sum
 }
